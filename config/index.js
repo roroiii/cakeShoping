4e2b4d5a72ec9a9e49f4db50ec26fe07fde737cb
Tina <x78350@gmail.com>
2021-12-04 13:25:57 +0800

    修改 config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-<<<<<<< HEAD
-export const server = dev
-  ? 'http://localhost:5003'
-  : 'https://secure-mountain-41419.herokuapp.com/';
-=======
 export const server = dev ? 'http://localhost:5003' : 'https://secure-mountain-41419.herokuapp.com';
->>>>>>> 93cee0c982e1340159c1f6937fa403bce5d55436
 
 export const web = dev ? 'http://localhost:3000' : 'https://yourwebsite.com';
