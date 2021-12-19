56145a0d3831db63c372b5229a4643dc9cdc3367
Tina <x78350@gmail.com>
2021-12-20 00:35:44 +0800

    修正api網址

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:5003'
-  : 'https://secure-mountain-41419.herokuapp.com/';
+  : 'https://secure-mountain-41419.herokuapp.com';
 
 export const web = dev ? 'http://localhost:3000' : 'https://yourwebsite.com';
