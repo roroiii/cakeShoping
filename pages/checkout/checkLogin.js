2319ebbe09d9f1cb8ea90eb3cf94382fd3980f53
Tina <x78350@gmail.com>
2022-02-27 05:54:51 +0800

    修正 checkout build 錯誤

---
+++ pages/checkout/checkLogin.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import LoginComponent from '../../components/LoginComponent';
+import { userCheckoutLogin } from '../../features/userSlice';
+import { useDispatch } from 'react-redux';
+
+export default function LoginPage() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  });
+  const [loginErrorMessage, setLoginErrorMessage] = useState(null);
+
+  const handleChangeUsername = (e) => {
+    setLoginErrorMessage(null);
+    setUsername(e.target.value);
+  };
+
+  const handleChangePassword = (prop) => (e) => {
+    setLoginErrorMessage(null);
+    setValues({ ...values, [prop]: e.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setLoginErrorMessage(null);
+    const payload = {
+      username,
+      password: values.password,
+    };
+    dispatch(userCheckoutLogin(router, payload)).then((res) => {
+      if (username === '' || values.password === '') {
+        return setLoginErrorMessage(`請輸入帳號密碼`);
+      }
+
+      if (res.data.ok === 0) {
+        return setLoginErrorMessage(res.data.message);
+      }
+    });
+  };
+  return (
+    <LoginComponent
+      username={username}
+      values={values}
+      loginErrorMessage={loginErrorMessage}
+      userText={`會員登入`}
+      handleChangeUsername={handleChangeUsername}
+      handleChangePassword={handleChangePassword}
+      handleClickShowPassword={handleClickShowPassword}
+      handleMouseDownPassword={handleMouseDownPassword}
+      handleLogin={handleLogin}
+    />
+  );
+}
